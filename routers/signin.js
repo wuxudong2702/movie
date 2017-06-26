559114be753b802290c7104860c356ccbafb3151
wuxudong <2713391431@qq.com>
2017-06-26 14:28:13 +0800

    gif
    上传gif演示图

--- routers/signin.js
+++ routers/signin.js
@@ -1,12 +1,12 @@
 /**
  * Created by wuxudong on 2017/6/23.
  */
-module.exports=     async (ctx, next) => {
+module.exports= async (ctx, next) => {
     "use strict";
     console.log("sign ctx.session.errorMessage",ctx.session.errorMessage)
-ctx.render('signin.html', {
-    errorMessage:ctx.session.errorMessage
-});
+    ctx.render('signin.html', {
+        errorMessage:ctx.session.errorMessage
+    });
 await next();
 }
 
